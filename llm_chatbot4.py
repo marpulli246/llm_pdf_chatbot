1709555cecad2903196e7a4e2271100c4e84f515
Markku Pulli <75437735+marpulli246@users.noreply.github.com>
2023-09-06 14:47:42 -0400

    Update llm_chatbot4.py

--- llm_chatbot4.py
+++ llm_chatbot4.py
@@ -86,29 +86,30 @@ def display_conversation(messages):
                 
 def get_conversation_chain(vector_store):
     llm = ChatOpenAI()
+
+    # HuggingFace Model
+
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
-        #chain_type = "stuff",
-        retriever = vector_store.as_retriever(), 
-        memory = memory,
-        #return_source_documents = True
-        #chain_type_kwargs={
-        #    "memory": ConversationBufferMemory(
-        #        memory_key='chat_history',
-        #        return_messages=True),
-        #}
+        retriever = vector_store.as_retriever(),
+        memory = memory
     )
-    return conversation_chain                
+
+    return conversation_chain             
 
 def handle_user_input(question):
     response = st.session_state.conversation({'question':question})
     st.session_state.chat_history = response['chat_history']
+
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-           st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)            
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
         else:
-           st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)                
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)                
             
 # Streamlit Frontend
 def main():

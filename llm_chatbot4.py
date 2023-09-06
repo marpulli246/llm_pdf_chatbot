25dc089afb3dbf462ad12413a3464ee4bf3bf392
Markku Pulli <75437735+marpulli246@users.noreply.github.com>
2023-09-06 10:47:30 -0400

    Add files via upload
    Multiple fixes

--- llm_chatbot4.py
+++ llm_chatbot4.py
@@ -96,7 +96,17 @@ def get_conversation_chain(vector_store):
         memory = memory
     )
     return conversation_chain                
-                
+
+def handle_user_input(question):
+
+    response = st.session_state.conversation({'question':question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)                
             
 # Streamlit Frontend
 def main():

4eca18fb82c786b7f97d8540d54b626eed89cd60
Markku Pulli <75437735+marpulli246@users.noreply.github.com>
2023-09-06 11:22:57 -0400

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,17 +28,17 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="/chatbot.png" width="100">
+        <img src="/chatbot.png" width="60">
     </div>
-    <div class="message">{{MSG}}</div>
+    
 </div>
 '''
 
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="/user.png" width="100">
+        <img src="/user.png" width="60">
     </div>    
-    <div class="message">{{MSG}}</div>
+    
 </div>
 '''

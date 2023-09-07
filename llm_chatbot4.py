26ee0c9906e51c5e5ab3654643c3ea8b2789ac91
Markku Pulli <75437735+marpulli246@users.noreply.github.com>
2023-09-07 11:30:08 -0400

    Update llm_chatbot4.py

--- llm_chatbot4.py
+++ llm_chatbot4.py
@@ -59,8 +59,7 @@ def handle_user_input(question):
             #st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
             st.markdown(
                 f'<div style="display: flex; align-items: center;">'
-                
-                f'<img src="C:\Users\marpulli\Downloads\chatbot.png" class="inline" style="width: 50px; height: 50px; margin-right: 10px;">'
+                f'<img src="/images/chatbot.png" alt="bot" class="inline" style="width: 50px; height: 50px; margin-right: 10px;">'
                 f'<div>{bot_template.replace("{{MSG}}", message.content)}</div>'
                 f'</div>',
                 unsafe_allow_html=True

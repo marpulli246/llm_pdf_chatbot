fecdc3ec53d19fbfa2ba60e47338739f5a8afed5
Markku Pulli <75437735+marpulli246@users.noreply.github.com>
2023-09-07 14:11:58 -0400

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -25,6 +25,20 @@ css = '''
 }
 '''
 
+html_code = """
+<style>
+  .fixed-text-input {
+    position: fixed;
+    top: 10px; /* Adjust top position as needed */
+    left: 10px; /* Adjust left position as needed */
+    z-index: 1000; /* Ensure it's above other content */
+  }
+</style>
+<div class="fixed-text-input">
+  <input type="text" placeholder="Enter text">
+</div>
+"""
+
 bot_template = '''
 <div class="chat-message bot">
     <div class="message">{{MSG}}</div> 

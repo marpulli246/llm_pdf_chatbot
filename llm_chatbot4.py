852180b5c49f29822ae06e75f39620ac4274c973
Markku Pulli <75437735+marpulli246@users.noreply.github.com>
2023-09-08 09:24:28 -0400

    Update llm_chatbot4.py

--- llm_chatbot4.py
+++ llm_chatbot4.py
@@ -59,12 +59,18 @@ def handle_user_input(question):
     #    message(st.session_state['generated'][::-1][i], key=str(i))
     #for i, message in enumerate(st.session_state.chat_history):
     reversed_chat_history = reversed(st.session_state.chat_history)
-    for i, message in enumerate(reversed_chat_history): 
-        #if i % 2 == 1:
-        st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+   # Display the chat history
+    for message in reversed_chat_history:
+        #chat_html = generate_chat_html(message)
+        st.write(message, unsafe_allow_html=True)
+    
+   # for i, message in enumerate(reversed_chat_history): 
+       # if i % 2 == 0:
+        #st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
             #st.write(message.content, unsafe_allow_html=True)
         #else:
-        st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        #st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
             #st.write(message.content, unsafe_allow_html=True)
             #st.markdown(
             #    f'<div style="display: flex; align-items: center;">'

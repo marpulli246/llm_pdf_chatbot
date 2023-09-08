a33ad173ab54104174cb2a54e5b1653ab31cda96
Markku Pulli <75437735+marpulli246@users.noreply.github.com>
2023-09-08 11:19:17 -0400

    Update llm_chatbot4.py

--- llm_chatbot4.py
+++ llm_chatbot4.py
@@ -51,18 +51,16 @@ def get_conversation_chain(vector_store):
 def handle_user_input(question):
     response = st.session_state.conversation({'question':question})
     st.session_state.chat_history = response['chat_history']
-    #st.write(len(reversed_chat_history), unsafe_allow_html=True)
-    #st.write(st.session_state.chat_history[0].content, unsafe_allow_html=True)
-    #st.write(reversed_chat_history[4], unsafe_allow_html=True)
-    #st.write(reversed_chat_history, unsafe_allow_html=True)
+    message = st.session_state.chat_history[0]
+    st.write(message.content)
     #Display the chat history
-    chat_length = len(st.session_state.chat_history)
-    for i in range(chat_length/2):
-        index = chat_length-(i*2+2)
-        message = st.session_state.chat_history[index]
-        st.write(user_template.replace("{{MSG}}", message.content, unsafe_allow_html=True)
-        message = st.session_state.chat_history[index+1]        
-        st.write(bot_template.replace("{{MSG}}", message.content, unsafe_allow_html=True)
+    #chat_length = len(st.session_state.chat_history)
+    #for i in range(chat_length/2):
+       # index = chat_length-(i*2+2)
+       # message = st.session_state.chat_history[index]
+        #st.write(user_template.replace("{{MSG}}", message.content, unsafe_allow_html=True)
+       # message = st.session_state.chat_history[index+1]        
+       # st.write(bot_template.replace("{{MSG}}", message.content, unsafe_allow_html=True)
     
     #for i, message in enumerate(reversed_chat_history): 
        # if i % 2 == 0:
